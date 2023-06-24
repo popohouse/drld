ddbc1b9898c9233dae2cdb5a69995769597aacf9
Popo <admin@popo.house>
2023-06-24 10:54:45 -0600

    slight improvements

--- cogs/repost.py
+++ cogs/repost.py
@@ -1,38 +1,44 @@
 import discord
 from discord.ext import commands
-
+import re
 
 
 class Repost(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
-        self.cache = []
+        self.tweet_cache = []
 
 
     async def setup(self):
-        await self.cache_reposts()
+        await self.cache_tweets()
 
-    async def cache_reposts(self):
+    async def cache_tweets(self):
         async with self.bot.pool.acquire() as conn:
-            query = "SELECT * FROM repost"
+            query = "SELECT * FROM tweet"
             rows = await conn.fetch(query)
             for row in rows:
                 post = row["post"]
-                self.cache.append(post)
+                self.tweet_cache.append(post)
 
     @commands.Cog.listener()
     async def on_message(self, message: discord.Message):
         """Saves twitter links to a database, compares if previously posted then automatically removes the message."""
         if message.author.bot:
             return
-        if "twitter.com" in message.content:
-                link = message.content
+        twitter_pattern = r'https?://(www\.)?(fxtwitter|vxtwitter|twitter)\.com/[^\s]+'
+        twitter = re.search(twitter_pattern, message.content)
+
+        if twitter:
+                link = twitter.group()
+                link = link.replace('fxtwitter.com', 'twitter.com')
+                link = link.replace('vxtwitter.com', 'twitter.com')
+                print(link)
                 split_message = link.split("?")
                 tweet = split_message[0]
-                if tweet in self.cache:
+                if tweet in self.tweet_cache:
                     await message.delete()
                 else:
-                    self.cache.append(tweet)
+                    self.tweet_cache.append(tweet)
                     async with self.bot.pool.acquire() as conn:
                         await conn.execute('INSERT INTO tweet (post) VALUES ($1)', tweet)
 
